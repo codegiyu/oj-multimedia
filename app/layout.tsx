8a2898ec030b3730a097aeaa59523a9eea50d685
Edward-Precious Omegbu <eomegbu@gmail.com>
2026-05-03 18:30:49 +0100

    fix: data-scroll-behaviour smooth added to html in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth">
       <body className={`antialiased`}>
         <NextTopLoader color="hsl(var(--primary))" height={3} showSpinner={false} />
         <ScrollRestorationHandler />
